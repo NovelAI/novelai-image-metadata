ba9aa0f4fcde69db09cd07d54278510a946c7c3c
Luna <git@l4.pm>
2026-02-04 18:38:40 -0300

    nai_sig: use faster LSBExtractor

--- nai_sig.py
+++ nai_sig.py
@@ -17,55 +17,10 @@ import io
 import sys
 import os
 import argparse
+from nai_meta import LSBExtractor
 
 ImageArray = NDArray[np.int8]
 log = logging.getLogger(__name__)
-
-
-class LSBExtractor:
-    def __init__(self, data: Any):
-        self.data = data
-        self.rows, self.cols, self.dim = data.shape
-        self.bits: int = 0
-        self.byte: int = 0
-        self.row: int = 0
-        self.col: int = 0
-
-    def _extract_next_bit(self):
-        if self.row < self.rows and self.col < self.cols:
-            bit = self.data[self.row, self.col, self.dim - 1] & 1
-            self.bits += 1
-            self.byte <<= 1
-            self.byte |= bit
-            self.row += 1
-            if self.row == self.rows:
-                self.row = 0
-                self.col += 1
-
-    def get_one_byte(self):
-        while self.bits < 8:
-            self._extract_next_bit()
-        byte = bytearray([self.byte])
-        self.bits = 0
-        self.byte = 0
-        return byte
-
-    def get_next_n_bytes(self, n: int):
-        bytes_list = bytearray()
-        for _ in range(n):
-            byte = self.get_one_byte()
-            if not byte:
-                break
-            bytes_list.extend(byte)
-        return bytes_list
-
-    def read_32bit_integer(self):
-        bytes_list = self.get_next_n_bytes(4)
-        if len(bytes_list) == 4:
-            integer_value = int.from_bytes(bytes_list, byteorder="big")
-            return integer_value
-        else:
-            return None
 
 
 @dataclass
